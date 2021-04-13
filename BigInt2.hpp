8c17d6bd9c9677c5dea9449c5e0c35435e7dc4af
Florent C. L. M. Michel <56166507+FlorentCLMichel@users.noreply.github.com>
2021-04-13 19:21:30 +0100

    Make nDigits and base private

--- BigInt2.hpp
+++ BigInt2.hpp
@@ -16,15 +16,18 @@ vector<unsigned long> toVec(string n, unsigned int nDigits);
 vector<unsigned long> toVec(unsigned long n, unsigned long base);
 
 class Bigint {
-	public:
-		
+    
+    private:
+        
         // number of base-10 digits per Bigint digit
 		unsigned int nDigits;
-
+        
         unsigned long base;
 		
-		// vector storing the value
+        // vector storing the value
 		vector<unsigned long> value;
+
+	public:
 
 		// convert a long unsigned int to a Bigint
 		explicit Bigint(unsigned long n, unsigned int nDigits = 10); 
@@ -52,39 +55,39 @@ class Bigint {
 	
 		// convert the value to a string
 		string toString(); 
+
+        friend bool isPrime(Bigint n); 
+
+        // get a random Bigint between 0 and N-1
+        friend Bigint randomBigint(Bigint N, mt19937 &rng);
+
+        // sum of the digits
+        friend Bigint sumDigits(Bigint N);
+
+        friend bool operator>(Bigint n1, Bigint n2);
+        friend bool operator==(Bigint n1, Bigint n2);
+        friend bool operator>=(Bigint n1, Bigint n2);
+        friend bool operator<(Bigint n1, Bigint n2);
+        friend bool operator<=(Bigint n1, Bigint n2);
+        friend ostream& operator<<(ostream& out, Bigint n);
+        friend Bigint operator+(Bigint n1, Bigint n2);
+        friend void operator+=(Bigint &n1, Bigint n2);
+        friend Bigint operator-(Bigint n1, Bigint n2); // assumes n2 <= n1
+        friend void operator-=(Bigint &n1, Bigint n2);
+        friend Bigint operator+(Bigint N, unsigned long n);
+        friend void operator+=(Bigint &N, unsigned long n);
+        friend Bigint operator+(unsigned long n, Bigint N);
+        friend Bigint operator*(unsigned long n, Bigint BN);
+        friend Bigint operator*(Bigint BN, unsigned long n);
+        friend Bigint operator*(Bigint n1, Bigint n2);
+        friend Bigint factorial(Bigint n); // factorial
+        friend Bigint pow(Bigint N, unsigned int n); // power
+        friend Bigint operator/(Bigint n1, Bigint n2);
+        friend Bigint operator%(Bigint n1, Bigint n2);
 };
 
-// definition of some operators
-bool operator>(Bigint n1, Bigint n2);
-bool operator==(Bigint n1, Bigint n2);
-bool operator>=(Bigint n1, Bigint n2);
-bool operator<(Bigint n1, Bigint n2);
-bool operator<=(Bigint n1, Bigint n2);
-ostream& operator<<(ostream& out, Bigint n);
-Bigint operator+(Bigint n1, Bigint n2);
-void operator+=(Bigint &n1, Bigint n2);
-Bigint operator-(Bigint n1, Bigint n2); // assumes n2 <= n1
-void operator-=(Bigint &n1, Bigint n2);
-Bigint operator+(Bigint N, unsigned long n);
-void operator+=(Bigint &N, unsigned long n);
-Bigint operator+(unsigned long n, Bigint N);
-Bigint operator*(unsigned long n, Bigint BN);
-Bigint operator*(Bigint BN, unsigned long n);
-Bigint operator*(Bigint n1, Bigint n2);
-Bigint factorial(Bigint n); // factorial
-Bigint pow(Bigint N, unsigned int n); // power
-Bigint operator/(Bigint n1, Bigint n2);
-Bigint operator%(Bigint n1, Bigint n2);
-
-// finding prime numbers
-bool isPrime(Bigint n); 
 Bigint nextPrime(Bigint n);
 vector<Bigint> firstPrimes(unsigned int n);
 
-// sum of the digits
-Bigint sumDigits(Bigint N);
-
-// get a random Bigint between 0 and N-1
-Bigint randomBigint(Bigint N, mt19937 &rng);
 
 #endif
